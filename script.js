693249cd010e75a6f50d5469e68da69660ed673d
jhonatan peña <jhonatan2002pb21@gmail.com>
2025-04-23 22:19:55 -0500

    Refactor opciones de menú: mejora en la estructura y funcionalidad de recursos y comunidad

--- script.js
+++ script.js
@@ -22,24 +22,69 @@ const resourcesSvg = document.querySelector(".resources-svg"); // Obtener elemen
 
 // La funcion que esta dentro de la funcion que escucha el evento se llama callback
 optResources.addEventListener("click", () => {
-  // optresources.children[0].classList.toggle("rotate-up"); // no recomendada
-  resourcesSvg.classList.toggle("rotate-up"); // recomendada
+  toggleClassRotateSvg(resourcesSvg);
   optResourcesContent.classList.toggle("show-opt-content");
 
   optCommunityContent.classList.remove("show-opt-content");
-  communitySvg.classList.remove("rotate-up");
+  removeClassRotateSvg(communitySvg);
 });
 
 // li - Opcion community
-
 const optCommunity = document.querySelector(".opt-community");
 const optCommunityContent = document.querySelector(".opt-community-content");
 const communitySvg = document.querySelector(".community-svg");
 
 optCommunity.addEventListener("click", () => {
-  communitySvg.classList.toggle("rotate-up");
+  toggleClassRotateSvg(communitySvg);
   optCommunityContent.classList.toggle("show-opt-content");
 
   optResourcesContent.classList.remove("show-opt-content");
-  resourcesSvg.classList.remove("rotate-up");
+  removeClassRotateSvg(resourcesSvg);
 });
+
+function toggleClassRotateSvg(elementSvg) {
+  elementSvg.classList.toggle("rotate-up");
+}
+
+function removeClassRotateSvg(elementSvg) {
+  elementSvg.classList.remove("rotate-up");
+}
+
+// Funcion de flecha
+// const saludar2 = () => {
+//   console.log("Saludando en la funcion de flecha");
+// };
+
+// Escucha de eventos para varios elementos - avanzado
+// const listOptionHeader = document.querySelectorAll(".opt-header");
+
+// listOptionHeader.forEach((liElement, key) => {
+//   liElement.addEventListener("click", () => {
+//     listOptionHeader.forEach((otherElement) => {
+//       if (otherElement != liElement) {
+//         otherElement.childNodes[1]?.classList.remove("rotate-up");
+//         otherElement.childNodes[3]?.classList.remove("show-opt-content");
+//       }
+//     });
+
+//     liElement.childNodes[1].classList.toggle("rotate-up");
+//     liElement.childNodes[3].classList.toggle("show-opt-content");
+//   });
+// });
+
+// Tipos de datos
+// string "los textos"
+// number 1234
+// float 1.5
+// boolean true o false
+
+// objects
+// const usuario = {
+//   nombre: "Jhonatan",
+//   apellido: "Peña",
+//   edad: 23,
+//   hobits: ["Jugar juegos", "pintar", "Bailar", 123, false, 1.9],
+// };
+
+// Array
+// const listPeliculas = ["jumanji", "titanic", "interesletaer"];
